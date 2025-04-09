bff987b465598e36c5d052b09baac4d55b8b983a
Leonel E. Bracco <leonelatm44@gmail.com>
2025-04-09 18:04:17 +0000

    refactor as asyncio

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
-#locals:
+import asyncio
 from game_loop import *
 from win_screen import *
 from select_character_screen import *
 from start_screen import *
 from load_global_variables import *
-
-# from MazeGame import *
-
-
 
 pygame.init()
 pygame.mixer.init()
@@ -21,16 +17,14 @@ clock = pygame.time.Clock()
 victory_img = pygame.image.load("./imagenes/corazon.png").convert_alpha()
 logo_img = pygame.image.load("./imagenes/cara_male_editada.png").convert_alpha()
 exit_img = pygame.image.load("./imagenes/pokeball.png").convert_alpha()
-
-
 exit_img = pygame.transform.scale(exit_img, (CELL_SIZE, CELL_SIZE))
 
-def main():
+async def main():
     while True:
-        width, height, player_name = start_screen(clock,logo_img)
+        width, height, player_name = start_screen(clock, logo_img)
         selected_player = select_character_screen(clock)
-        game_loop(width, height, player_name,selected_player,exit_img,victory_img,clock)
-
+        game_loop(width, height, player_name, selected_player, exit_img, victory_img, clock)
+        await asyncio.sleep(0)  # Give control back to the main thread
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
